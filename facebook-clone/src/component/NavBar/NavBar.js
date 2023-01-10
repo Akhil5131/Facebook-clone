01788ef4206717effea3a14565585104b211af2f
Akhil5131 <113959109+Akhil5131@users.noreply.github.com>
2023-01-10 14:22:49 -0600

    first commit

---
+++ facebook-clone/src/component/NavBar/NavBar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import "./NavBar.css";
+import { Grid } from '@material-ui/Grid';
+class  NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    
+    render() { 
+        return ( 
+           <div>
+            <Grid container>
+                <Grid item xs =  {3}>
+                    left
+                </Grid>
+                <Grid item xs =  {6}>
+                    middle
+                </Grid>
+                <Grid item xs = {3}>
+                    right
+                </Grid>
+
+            </Grid>
+           </div>
+         );
+    }
+}
+ 
+export default NavBar;
